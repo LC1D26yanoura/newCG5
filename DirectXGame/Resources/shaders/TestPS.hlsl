c6c89b3c62258f5b6d4400d7e0fcac60198a09f3
LE2D22yanoura <k023g0120@g.neec.ac.jp>
2025-09-16 23:31:51 +0900

    完了

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -1,16 +1,17 @@
-//float4 main() : SV_TARGET
-//{
-//    return float4(1.0f, 1.0f, 1.0f, 1.0f);
-//}
+#include "Test.hlsli"
 
-struct PixelShederOutput
+struct PixelShaderOutput
 {
-    float32_t4 color : SV_Target0;
+    float32_t4 color : SV_TARGET0;
 };
 
-PixelShederOutput main()
+
+PixelShaderOutput main(VertexShaderOutput input)
 {
-    PixelShederOutput output;
-    output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
+    PixelShaderOutput output;
+    float32_t2 uv = input.texcoord;
+
+    output.color = float32_t4(uv.x, uv.y, 0.0f, 1.0f);
+
     return output;
 }
